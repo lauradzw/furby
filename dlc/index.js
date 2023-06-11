e9287f2b886f9ef625bf30f3d8f1f8031d29e67b
lauradzw <136252686+lauradzw@users.noreply.github.com>
2023-06-11 21:09:54 +0200

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -7,11 +7,25 @@
         ]
     },
     { 
-        "file": "monsterdrinklady.dlc", 
-        "title": "MONSTER",
+        "file": "hacked.dlc", 
+        "title": "HACKED",
         "buttons": [
-            { "title": "MONSTER", "action": [75,0,3,4] },
-    
+            { "title": "Hacked 1", "action": [75,0,3,4] },
+            { "title": "Hacked 2", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "monsterdrinklady.dlc", 
+        "title": "Monster",
+        "buttons": [
+            { "title": "Monster test ", "action": [75,0,0,3] }
+        ]
+    },
+    {
+        "file": "chilli.dlc", 
+        "title": "Chilli",
+        "buttons": [
+            { "title": "Chilli ", "action": [75,0,4,4] }
         ]
     }
 ]
