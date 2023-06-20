a4696bde34b0870444815ac190f3ec85d94c171e
lauradzw <136252686+lauradzw@users.noreply.github.com>
2023-06-20 21:52:10 +0200

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -1,31 +1,100 @@
 [
     { 
-        "file": "context.dlc", 
-        "title": "Furb test",
+        "file": "alexjones.dlc", 
+        "title": "alexjones",
         "buttons": [
-            { "title": "Furb test", "action": [75,0,4,4] }
+            { "title": "alexjones", "action": [75,0,4,4] }
         ]
     },
     { 
-        "file": "hacked.dlc", 
-        "title": "HACKED",
+        "file": "anime.dlc", 
+        "title": "anime",
         "buttons": [
-            { "title": "Hacked 1", "action": [75,0,3,4] },
-            { "title": "Hacked 2", "action": [75,0,4,4] }
+            { "title": "anime", "action": [75,0,3,4] },
+            { "title": "anime", "action": [75,0,4,4] }
         ]
     },
     {
-        "file": "monsterdrinklady.dlc", 
-        "title": "Monster",
+        "file": "balenciagas.dlc", 
+        "title": "balenciagas",
         "buttons": [
-            { "title": "Monster test ", "action": [75,0,0,3] }
+            { "title": "balenciagas ", "action": [75,0,0,3] }
         ]
     },
     {
-        "file": "chilli.dlc", 
-        "title": "Chilli",
+        "file": "kanye.dlc", 
+        "title": "kanye",
         "buttons": [
-            { "title": "Chilli ", "action": [75,0,4,4] }
+            { "title": "kanye ", "action": [75,0,4,4] }
         ]
-    }
+    },
+    { 
+        "file": "kids.dlc", 
+        "title": "kids",
+        "buttons": [
+            { "title": "kids", "action": [75,0,4,4] }
+        ]
+    },
+    { 
+        "file": "monster.dlc", 
+        "title": "monster",
+        "buttons": [
+            { "title": "monster", "action": [75,0,3,4] },
+        ]
+    },
+    {
+        "file": "pokemon.dlc", 
+        "title": "pokemon",
+        "buttons": [
+            { "title": "pokemon", "action": [75,0,0,3] }
+        ]
+    },
+    {
+        "file": "senator1.dlc", 
+        "title": "senator1",
+        "buttons": [
+            { "title": "senator1 ", "action": [75,0,4,4] }
+        ]
+    },
+    { 
+        "file": "senator2.dlc", 
+        "title": "senator2",
+        "buttons": [
+            { "title": "senator2", "action": [75,0,4,4] }
+        ]
+    },
+    { 
+        "file": "senator3.dlc", 
+        "title": "senator3",
+        "buttons": [
+            { "title": "senator3", "action": [75,0,3,4] },
+        ]
+    },
+    {
+        "file": "teletubbies.dlc", 
+        "title": "teletubbies",
+        "buttons": [
+            { "title": "teletubbies", "action": [75,0,0,3] }
+        ]
+    },
+    {
+        "file": "tiktok01.dlc", 
+        "title": "tiktok01",
+        "buttons": [
+            { "title": "tiktok01 ", "action": [75,0,4,4] }
+        ]
+            {
+        "file": "tiktok02.dlc", 
+        "title": "tiktok02",
+        "buttons": [
+            { "title": "tiktok02 ", "action": [75,0,4,4] }
+        ]
+    },
+    { 
+        "file": "visualsandtravis.dlc", 
+        "title": "visualsandtravis",
+        "buttons": [
+            { "title": "visualsandtravis", "action": [75,0,4,4] }
+        ]
+     }
 ]
