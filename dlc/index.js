e6e715d78a050769ca8c3d8858268601b9e9ddf3
lauradzw <136252686+lauradzw@users.noreply.github.com>
2023-06-21 17:52:18 +0200

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -3,14 +3,14 @@
         "file": "alexjones.dlc", 
         "title": "alexjones",
         "buttons": [
-            { "title": "alexjones", "action": [75,0,4,4] }
+            { "title": "alexjones", "action": [75,0,0,3] }
         ]
     },
     { 
         "file": "visualsandtravis.dlc", 
         "title": "visualsandtravis",
         "buttons": [
-            { "title": "visualsandtravis", "action": [75,0,4,4] }
+            { "title": "visualsandtravis", "action": [75,0,0,3] }
         ]
      }
 ]
