3ecf6df6d875edb75d36af8a1563b62a7eefa282
lauradzw <136252686+lauradzw@users.noreply.github.com>
2023-06-23 09:39:09 +0200

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -1,30 +1,9 @@
 [
     { 
-        "file": "alex.dlc", 
-        "title": "alex",
+        "file": "alexandkylie.dlc", 
+        "title": "alexandkylie",
         "buttons": [
-            { "title": "alex", "action": [75,0,4,5] }
+            { "title": "alexandkylie", "action": [75,0,0,2] }
         ]
-    },
-    { 
-        "file": "bal.dlc", 
-        "title": "balenciaga",
-        "buttons": [
-            { "title": "balenciaga", "action": [75,0,5,6] }
-        ]
-     },
-     { 
-        "file": "ye.dlc", 
-        "title": "kanye",
-        "buttons": [
-            { "title": "ye", "action": [75,0,6,5] }
-        ]
-    },
-    { 
-        "file": "teletubbies.dlc", 
-        "title": "teletubbies",
-        "buttons": [
-            { "title": "teletubbies", "action": [75,0,4,6] }
-        ]
-     }
+    }
 ]
