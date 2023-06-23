4152c75db76b171f87dd30424ca4bdd3c678344b
lauradzw <136252686+lauradzw@users.noreply.github.com>
2023-06-23 11:35:48 +0200

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -1,16 +1,16 @@
 [
     {
-        "file": "monsterlady.dlc", 
-        "title": "monsterlady",
-        "buttons": [
-            { "title": "monsterlady", "action": [75,0,0,2] } 
-        ]
-    },
-    {
         "file": "alexsh.dlc", 
         "title": "alex short",
         "buttons": [
             { "title": "alex short", "action": [75,0,0,2] } 
+        ]
+    },
+    {
+        "file": "monsterlady.dlc", 
+        "title": "monsterlady",
+        "buttons": [
+            { "title": "monsterlady", "action": [75,0,0,2] } 
         ]
     }
 ]
