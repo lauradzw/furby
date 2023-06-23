3cb8cd585562b9aacbe6310cbee38f49363d9562
lauradzw <136252686+lauradzw@users.noreply.github.com>
2023-06-23 09:54:41 +0200

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -3,7 +3,14 @@
         "file": "alexandkylie.dlc", 
         "title": "alex j",
         "buttons": [
-            { "title": "alex", "action": [75,0,0,2] } 
+            { "title": "alex and kylie", "action": [75,0,0,2] } 
+        ]
+    },
+    {
+        "file": "alexshort.dlc", 
+        "title": "alex short",
+        "buttons": [
+            { "title": "alex short", "action": [75,0,0,2] } 
         ]
     }
 ]
